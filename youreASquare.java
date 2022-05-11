5a4aa231061de69de9d26a16333d4061312e6e14
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-05-11 18:37:37 +0100

    Create youreASquare.java

---
+++ youreASquare.java
@@ -0,0 +1,9 @@
+// You're a square!
+
+public class Square {    
+    public static boolean isSquare(int n) {        
+//         Check if n can be square rooted. If yes, true. If no, false.
+      
+    return Math.sqrt(n) == (int) Math.sqrt(n);
+  }
+}
